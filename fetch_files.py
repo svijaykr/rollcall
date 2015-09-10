d04a70fcf79943c40390dc145ecb05b9d905c7bf
Siddharth Vijayakrishnan <svijaykr@gmail.com>
2015-09-09 22:19:35 -0700

    actually fetches files : single process

--- fetch_files.py
+++ fetch_files.py
@@ -1,4 +1,37 @@
 import sys 
+import urllib2
+
+def fetch_list(file_name):
+    f = open(file_name)
+    for line in f:
+        line = line.strip('\n')
+        contents = fetch_single_file(line)
+        if contents is not None:
+            file_name = make_output_file_name(line)
+            write_single_file(file_name, contents)
+            print line, ": DONE"
+        else:
+            print line, ": FAILED"
+    f.close()
+
+def fetch_single_file(url):
+    SUCCESS = 200
+    f = urllib2.urlopen(url)
+    content = f.read()
+    code = f.getcode()
+    if (code != SUCCESS):
+        return None
+    return content
+
+def make_output_file_name(url):
+    url_parts = url.split('/')
+    file_name = url_parts[len(url_parts) - 1]
+    return file_name
+
+def write_single_file(file_name,content):
+    f = open(file_name,'w')
+    f.write(content)
+    f.close()
 
 def make_chunk(lines,noOfLines,noOfProcesses):
     filesPerProcess = noOfLines / noOfProcesses 
@@ -18,8 +51,8 @@ def make_chunk(lines,noOfLines,noOfProcesses):
     chunks.append(chunk)
     return chunks
 
-def write_chunk(chunk, chunkFileName):
-    f = open(chunkFileName,'w')
+def write_chunk(chunk, chunk_file_name):
+    f = open(chunk_file_name,'w')
     for lines in chunk:
         f.write(lines)
     f.close()
@@ -28,14 +61,18 @@ def read_master_file(fname):
     f = open(fname,'r')
     return f.readlines()
 
-
-if __name__ == '__main__':
-    print sys.argv
+def apportion():
     lines = read_master_file(sys.argv[1])
     noOfProcesses = int(sys.argv[2])
     chunks = make_chunk(lines, len(lines),noOfProcesses)
+    out_file_list = []
     for i,chunk in enumerate(chunks):
-        write_chunk(chunk,'tmp_chunk'+ str(i) + '.txt')
+        out_file_name = 'tmp_chunk'+ str(i) + '.txt'
+        write_chunk(chunk,out_file_name)
+        out_file_list.append(out_file_name)
+    return out_file_list
 
 
-
+if __name__ == '__main__':
+    out_file_list = apportion()
+    fetch_list(out_file_list[0])
--- goa_make_list.py
+++ goa_make_list.py
@@ -1,8 +1,12 @@
-f = open('goa_master_list.txt','r')
+f = open('goa_seed.txt','r')
 prefix = 'http://ceogoa.nic.in/PDF/EROLL/2015/AC'
 suffix1 = '/Part'
 suffix2 = '.pdf'
 for lines in f:
     (cons_id, last_no) = lines.strip('\n').split()
     for i in xrange(1,int(last_no)):
-        print prefix + cons_id + suffix1 + str(i)  + suffix2
+        if i < 10:
+            part_no  = '0' + str(i)
+        else:
+            part_no = str(i)
+        print prefix + cons_id + suffix1 + part_no  + suffix2
